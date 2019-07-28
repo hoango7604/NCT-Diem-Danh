50d1e0fb655cc257c15d93fb015a3386fd66a4ac
hoango7604 <30996727+hoango7604@users.noreply.github.com>
2019-07-28 13:39:44 +0700

    Initialized repository

---
+++ rest/shared/share_modules.js
@@ -0,0 +1,45 @@
+/**
+ * Imports modules for public uses in this project
+ */
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const Knex = require('knex');
+const config = require('config');
+const { Model } = require('objection');
+const { validateBodyInput, validateQueryInput, schemas } = require('../helpers/route_helpers');
+
+const knexConnection = Knex(config.get('db_config'));
+Model.knex(knexConnection);
+
+const TABLE_NAME = {
+	vitri: 'doan_vitri',
+	nhanvien: 'doan_nhanvien',
+	calamviec: 'doan_calamviec',
+	bangchiaca: 'doan_bangchiaca',
+	danhmucmon: 'doan_danhmucmon',
+	monan: 'doan_monan',
+	banan: 'doan_banan',
+	phieuchonmon: 'doan_phieuchonmon',
+	chitietphieuchonmon: 'doan_chitietphieuchonmon',
+	hoadon: 'doan_hoadon'
+}
+
+const JWT_SECRET = config.get('JWT_SECRET');
+
+app.use(bodyParser.json());
+
+module.exports = {
+	express,
+	cors,
+	bodyParser,
+	app,
+	validateBodyInput,
+	validateQueryInput,
+	schemas,
+	Model,
+
+	TABLE_NAME,
+	JWT_SECRET
+}
